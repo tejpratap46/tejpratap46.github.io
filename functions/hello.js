6235c3b1e398f01eb140535f390bbb30a42d6d39
Tej Pratap Sngh <tejpratap46@gmail.com>
2020-06-10 21:22:17 +0530

    Added comment to functions

--- functions/hello.js
+++ functions/hello.js
@@ -1,4 +1,5 @@
 exports.handler = function(event, context, callback) {
+    // Call at: https://me.tejpratapsingh.com/.netlify/functions/hello
     callback(null, {
         statusCode: 200,
         body: "Hello, World"
