1a28bfb3d482f8dfacaf772cc8a505ce14bf4c36
Tej Pratap Sngh <tejpratap46@gmail.com>
2020-07-03 23:54:54 +0530

    updated function pixle

--- functions/pixel.js
+++ functions/pixel.js
@@ -1,7 +1,9 @@
 exports.handler = function(event, context, callback) {
     // Call at: https://me.tejpratapsingh.com/.netlify/functions/pixel
     callback(null, {
-        statusCode: 200,
-        body: "Hello, World"
+        statusCode: 301,
+        headers: {
+            Location: "https://resume.tej.workers.dev"
+        }
     });
 };
