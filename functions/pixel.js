d867686468907ffb43d27b38fd65cf1aa6ee0295
Tej Pratap Sngh <tejpratap46@gmail.com>
2020-07-03 23:14:43 +0530

    updated function pixel

--- functions/pixel.js
+++ functions/pixel.js
@@ -1,5 +1,5 @@
 exports.handler = function(event, context, callback) {
-    // Call at: https://me.tejpratapsingh.com/.netlify/functions/pixel.jpg
+    // Call at: https://me.tejpratapsingh.com/.netlify/functions/pixel
     callback(null, {
         statusCode: 200,
         body: "Hello, World"
