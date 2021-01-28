4866c183a3e240ef522c6306941871a9b558cb18
tejpratap46 <tejpratap46@gmail.com>
2021-01-28 23:49:40 +0530

    remember dark mode

--- sw.js
+++ sw.js
@@ -1,18 +1,117 @@
-self.addEventListener("install", e => {
-    self.skipWaiting();
+// Set a name for the current cache
+var cacheName = 'v2.1'; 
+
+// Default files to always cache
+var cacheFiles = [
+	'./',
+	'./index.html',
+	'./assets/css/chota.css',
+	'./assets/css/style.css',
+	'./assets/css/tooltip.css',
+    './assets/images/tps.svg',
+    
+	'./assets/images/myopdzip.png',
+    './assets/images/ponto.png',
+    './assets/images/linkanalytics.png',
+    './assets/images/tlj.png',
+    './assets/images/invesys.png',
+    
+	'https://emoji-css.afeld.me/emoji.css',
+	'https://fonts.googleapis.com/css?family=Source%20Code%20Pro'
+]
+
+
+
+self.addEventListener('install', function(e) {
+    // console.log('[ServiceWorker] Installed');
+
+    // e.waitUntil Delays the event until the Promise is resolved
+    e.waitUntil(
+
+    	// Open the cache
+	    caches.open(cacheName).then(function(cache) {
+
+	    	// Add all the default files to the cache
+			// console.log('[ServiceWorker] Caching cacheFiles');
+			return cache.addAll(cacheFiles);
+	    })
+	); // end e.waitUntil
 });
 
-self.addEventListener("activate", e => {
-    return self.clients.claim();
+
+self.addEventListener('activate', function(e) {
+    // console.log('[ServiceWorker] Activated');
+
+    e.waitUntil(
+
+    	// Get all the cache keys (cacheName)
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(cacheNames.map(function(thisCacheName) {
+
+				// If a cached item is saved under a previous cacheName
+				if (thisCacheName !== cacheName) {
+
+					// Delete that cached file
+					// console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+					return caches.delete(thisCacheName);
+				}
+			}));
+		})
+	); // end e.waitUntil
+
 });
 
-self.addEventListener("fetch", e => {
-    // DevTools opening will trigger these o-i-c requests, which this SW can't handle.
-    if (
-        e.request.cache === "only-if-cached" &&
-        e.request.mode !== "same-origin"
-    )
-        return;
 
-    e.respondWith(fetch(e.request));
+self.addEventListener('fetch', function(e) {
+	// console.log('[ServiceWorker] Fetch', e.request.url);
+
+	// e.respondWidth Responds to the fetch event
+	e.respondWith(
+
+		// Check in cache for the request being made
+		caches.match(e.request)
+
+
+			.then(function(response) {
+
+				// If the request is in the cache
+				if ( response ) {
+					// console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+					// Return the cached version
+					return response;
+				}
+
+				// If the request is NOT in the cache, fetch and cache
+
+				var requestClone = e.request.clone();
+				fetch(requestClone)
+					.then(function(response) {
+
+						if ( !response ) {
+							// console.log("[ServiceWorker] No response from fetch ")
+							return response;
+						}
+
+						var responseClone = response.clone();
+
+						//  Open the cache
+						caches.open(cacheName).then(function(cache) {
+
+							// Put the fetched response in the cache
+							cache.put(e.request, responseClone);
+							// console.log('[ServiceWorker] New Data Cached', e.request.url);
+
+							// Return the response
+							return response;
+			
+				        }); // end caches.open
+
+					})
+					.catch(function(err) {
+						// console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+					});
+
+
+			}) // end caches.match(e.request)
+	); // end e.respondWith
 });
