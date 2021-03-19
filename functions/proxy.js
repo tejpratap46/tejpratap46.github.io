bf375c8c2b02fcbf74cab30711c1d6a1cf91a7a5
tejpratap46 <tejpratap46@gmail.com>
2021-03-19 12:29:26 +0530

    added url to proxy

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,42 +1,56 @@
-const http = require('https')
+const http = require('https');
+// Call at: https://me.tejpratapsingh.com/.netlify/functions/hello
 exports.handler = async (event) => {
+    const queryParameters = event.queryStringParameters;
+    const url = queryParameters.url;
+    if (!url) {
+        const response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: "Url missing"
+            }),
+        };
+        return response;
+    }
     return httprequest().then((data) => {
         const response = {
             statusCode: 200,
             body: JSON.stringify(data),
         };
-    return response;
+        return response;
     });
 };
-function httprequest() {
-     return new Promise((resolve, reject) => {
+function httprequest(url) {
+    const host = utl.split("/")[0];
+    const path = utl.split("/")[1];
+    return new Promise((resolve, reject) => {
         const options = {
-            host: 'jsonplaceholder.typicode.com',
-            path: '/todos',
+            host: host,
+            path: path,
             port: 443,
-            method: 'GET'
+            method: "GET"
         };
         const req = http.request(options, (res) => {
-          if (res.statusCode < 200 || res.statusCode >= 300) {
+            if (res.statusCode < 200 || res.statusCode >= 300) {
                 return reject(new Error('statusCode=' + res.statusCode));
             }
             var body = [];
-            res.on('data', function(chunk) {
+            res.on('data', function (chunk) {
                 body.push(chunk);
             });
-            res.on('end', function() {
+            res.on('end', function () {
                 try {
                     body = JSON.parse(Buffer.concat(body).toString());
-                } catch(e) {
+                } catch (e) {
                     reject(e);
                 }
                 resolve(body);
             });
         });
         req.on('error', (e) => {
-          reject(e.message);
+            reject(e.message);
         });
         // send the request
-       req.end();
+        req.end();
     });
 }
