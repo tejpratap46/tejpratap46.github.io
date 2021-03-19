cb641e8f1095db0fba2706aa9c026894b80fbd36
tejpratap46 <tejpratap46@gmail.com>
2021-03-19 12:18:50 +0530

    proxy test

---
+++ functions/proxy.js
@@ -0,0 +1,42 @@
+const http = require('https')
+exports.handler = async (event) => {
+    return httprequest().then((data) => {
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    return response;
+    });
+};
+function httprequest() {
+     return new Promise((resolve, reject) => {
+        const options = {
+            host: 'https://jsonplaceholder.typicode.com',
+            path: '/todos',
+            port: 443,
+            method: 'GET'
+        };
+        const req = http.request(options, (res) => {
+          if (res.statusCode < 200 || res.statusCode >= 300) {
+                return reject(new Error('statusCode=' + res.statusCode));
+            }
+            var body = [];
+            res.on('data', function(chunk) {
+                body.push(chunk);
+            });
+            res.on('end', function() {
+                try {
+                    body = JSON.parse(Buffer.concat(body).toString());
+                } catch(e) {
+                    reject(e);
+                }
+                resolve(body);
+            });
+        });
+        req.on('error', (e) => {
+          reject(e.message);
+        });
+        // send the request
+       req.end();
+    });
+}
