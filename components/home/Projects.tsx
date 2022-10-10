d787e0d258bbe6e66ca873e3013282016ac3a175
tejpratapsingh <tejpratap46@gmail.com>
2022-10-11 02:05:32 +0530

    added projects

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -1,28 +1,46 @@
 import Section from '@/components/section'
-import Image from 'next/image'
+
+const projectList = [
+	{
+		name: 'MyOPD Zip',
+		image: '/images/myopdzip.webp',
+		description: 'Power of MyOPD desktop suite compressed for mobile.',
+		url: 'https://play.google.com/store/apps/details?id=in.myopd.android'
+	},
+	{
+		name: 'MyOPD Appointments',
+		image: '/images/myopdzip.webp',
+		description: 'Power of MyOPD desktop suite compressed for mobile.',
+		url: 'https://play.google.com/store/apps/details?id=in.myopd.android'
+	}
+]
 
 const Projects = () => (
 	<Section>
 		<h3 className='text-xl text-gray-400 dark:text-gray-600 capitalize'><strong>4. SOME THINGS I&apos;VE BUILD </strong></h3>
 		<br />
-		<div className='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
-			<div className='bg-cover bg-center shadow-inner dark:bg-zinc-800 grayscale hover:filter-none transition ease-in-out duration-500'
-				style={{
-					height: 300,
-					width: '50%',
-					backgroundImage:
-						'url(/images/myopdzip.webp)',
-				}}></div>
-			<div className='p-5'>
-				<a href='#'>
-					<h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>MyOPD Zip</h5>
-				</a>
-				<p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Power of MyOPD desktop suite compressed for mobile.</p>
-				<a href='#' className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
-					Read more
-					<svg aria-hidden='true' className='ml-2 -mr-1 w-4 h-4' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z' clipRule='evenodd'></path></svg>
-				</a>
-			</div>
+		<div className='grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4'>
+			{
+				projectList.map((project) => {
+					return <div key={project.name} className='max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+					<div className='bg-cover bg-top shadow-inner'
+						style={{
+							height: 300,
+							backgroundImage: `url(${project.image})`,
+						}}></div>
+					<div className='p-5'>
+						<a>
+							<h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{project.name}</h5>
+						</a>
+						<p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{project.description}</p>
+						<a href={project.url} className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							Read more
+							<svg aria-hidden='true' className='ml-2 -mr-1 w-4 h-4' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z' clipRule='evenodd'></path></svg>
+						</a>
+					</div>
+				</div>
+				})
+			}
 		</div>
 
 	</Section>
