7cf1f19c6d41a7e1cd9b7dad7230b2d97539212a
Tej Pratap Singh <tejpratap46@gmail.com>
2023-02-01 11:47:02 +0530

    Added Index to Urls

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -109,8 +109,8 @@ const Projects = () => (
 							</a>
 							<p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{project.description}</p>
 							{
-								project.urls.map((item) => {
-									return <a href={item.url} target='_blank' rel='noreferrer' className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+								project.urls.map((item, index) => {
+									return <a key={index} href={item.url} target='_blank' rel='noreferrer' className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
 										<span>{item.name}</span>
 										<svg aria-hidden='true' className='ml-2 -mr-1 w-4 h-4' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z' clipRule='evenodd'></path></svg>
 									</a>
@@ -135,8 +135,8 @@ const Projects = () => (
 						</a>
 						<p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{project.description}</p>
 						{
-							project.urls.map((item) => {
-								return <a href={item.url} target='_blank' rel='noreferrer' className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							project.urls.map((item, index) => {
+								return <a key={index} href={item.url} target='_blank' rel='noreferrer' className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
 									<span>{item.name}</span>
 									<svg aria-hidden='true' className='ml-2 -mr-1 w-4 h-4' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z' clipRule='evenodd'></path></svg>
 								</a>
@@ -160,8 +160,8 @@ const Projects = () => (
 						</a>
 						<p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{project.description}</p>
 						{
-							project.urls.map((item) => {
-								return <a href={item.url} target='_blank' rel='noreferrer' className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+							project.urls.map((item, index) => {
+								return <a key={index} href={item.url} target='_blank' rel='noreferrer' className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
 									<span>{item.name}</span>
 									<svg aria-hidden='true' className='ml-2 -mr-1 w-4 h-4' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z' clipRule='evenodd'></path></svg>
 								</a>
