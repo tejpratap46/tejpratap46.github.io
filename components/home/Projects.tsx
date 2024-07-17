8c17291a554e8d1ebd6a02aad90fd4f2ac5cce15
Tej Pratap Singh <tejpratap46@gmail.com>
2024-07-17 14:05:38 +0530

    updated projects

--- components/home/Projects.tsx
+++ components/home/Projects.tsx
@@ -171,6 +171,7 @@ const Projects = () => (
 								<h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{project.name}</h5>
 							</a>
 							<p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{project.description}</p>
+							<div>
 							{
 								project.urls.map((item, index) => {
 									return <a key={index} href={item.url} target='_blank' rel='noreferrer' className='inline-flex items-center py-2 px-3 mx-2 my-1 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
@@ -179,6 +180,7 @@ const Projects = () => (
 									</a>
 								})
 							}
+							</div>
 						</div>
 					})
 				}
